11c127e0426435b5306872587206bbb539348b05
Charles Foster <charlesfoster@charless-air-4.music.wmgis.com>
2017-08-21 18:15:22 -0700

    Added universal report generator

--- generate_report.py
+++ generate_report.py
@@ -1,131 +1,207 @@
-import argparse, json, csv
+import os, argparse, json, csv
+
+GENRES = ['URBAN', 'ADULT CONTEMP', 'ROCK', 'POP', 'ELECTRONIC']
+SIZES = ['0-10K', '10K-100K', '100K-250K', '250K-500K', '500K-2M', '2M+']
+LIMITS = [0, 10000, 100000, 250000, 500000, 2000000] # Minimum limit for each size bucket
 
 def main():
 
-	# Set crawler target and parameters.
+	"""
+	Arguments:
+		inputs: A .csv file with the structure '[Genres], [Artist Name], [Artist Username] ...'
+		results: A .csv name for the results to be placed in.
+	"""
+
 	parser = argparse.ArgumentParser()
 
 	parser.add_argument("inputs", help="Set the .csv file from which inputs should be read.")
+	parser.add_argument("data", help="Set the directory profile data can be read from.")
 	parser.add_argument("results", help="Set the .csv file where the results should be stored.")
+	parser.add_argument("platform", type=str, choices=['facebook', 'instagram', 'youtube'])
+	#SKETU(JGNEHRE)
 
 	args = parser.parse_args()
 
-	genres = []
-	names = []
-	usernames = []
+	genres, names, usernames = read_profile_data(args.inputs, args.data, args.platform)
 
-	with open(args.inputs, 'r') as csvfile:
+	all_genres, all_sizes, all_engagements, all_ratios, stats = calculate_stats(genres, names, usernames, args.data, args.platform)
+
+	save_stats(args.results, all_genres, all_sizes, all_engagements, all_ratios, stats)
+
+def read_profile_data(fname, dirname, platform):
+	
+	"""
+	Arguments:
+		fname: The name of a file with profile data, extracted as a JSON object through our youtube_crawler.py script.
+		dirname: The name of the directory profile data can be read from.
+		platform: The name of the platform the data comes from.
+	"""
+
+	with open(fname, 'r') as csvfile:
+		genres = []
+		names = []
+		usernames = []
+
 		reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+		
 		for row in reader:
-			genres.append(row[0])
-			names.append(row[1])
-			usernames.append(row[2])
+			if os.path.exists(os.getcwd() + '/' + dirname + '/' + platform + '/' + row[1] + '.json'):
+				profile_genres = []
+				for genre in row[0].split('/'):
+					if genre in GENRES:
+						profile_genres.append(genre)
+				genres.append(profile_genres)
+				names.append(row[1])
+				usernames.append(row[2])
 
-	sizes = []
-	nposts = []
-	engagements = []
+		return genres, names, usernames
 
-	genrebuckets = ['URBAN', 'ADULT CONTEMP', 'ROCK', 'POP']
-	sizebuckets = ['0-10K', '10K-100K', '100K-250K', '250K-500K', '500K-2M', '2M+', 'None']
-	lowerlimits = [0, 10000, 100000, 250000, 500000, 2000000]
+def post_stats(post, size, platform):
 
-	npostslists = {}
-	engagementslists = {}
+	"""
+	Arguments:
+		post: A JSON object of the post to return engagement stats for.
+		size: Size of the profile's following.
+		platform: The name of the platform the data comes from.
+	"""
 
-	avgnposts = {}
-	avgengagements = {}
+	if platform == 'facebook':
+		engagements = post['shares'] + post['comments']
+		for reaction in post['reactions']:
+			engagements += post['reactions'][reaction]
+		ratio = engagements / float(size)
+	elif platform == 'instagram':
+		engagements = post['likes'] + post['comments']
+		ratio = engagements / float(size)
+	elif platform == 'youtube':
+		engagements = post['likes'] + post['dislikes'] + post['dislikes'] + post['comments'] + post['favorites']
+		if post['views'] is 0:
+			ratio = None
+		else:
+			ratio = engagements / float(post['views'])
+	else:
+		raise NotImplementedError
+	
+	return engagements, ratio
 
-	for genre in genrebuckets:
-		npostslists[genre] = {}
-		engagementslists[genre] = {}
+def profile_stats(name, dirname, platform):
 
-		avgnposts[genre] = {}
-		avgengagements[genre] = {}
-		for size in sizebuckets:
-			npostslists[genre][size] = []
-			engagementslists[genre][size] = []
+	"""
+	Arguments:
+		name: The name (as a string) of the artist whose stats should be returned.
+		dirname: The name of the directory profile data can be read from.
+		platform: The name of the platform the data comes from.
+	"""
 
-	for name, genrelist in zip(names, genres):
-		with open('results/facebook/' + name + '.json') as data_file:
-			data = json.load(data_file)
-			sizes.append(data['Follower_Count'])
+	with open(os.getcwd() + '/' + dirname + '/' + platform + '/' + name + '.json') as f:
+		data = json.load(f)
 
-			sizebucket = None
-			if data['Follower_Count'] is None:
-				sizebucket = sizebuckets[6]
-			else:
-				for ll in lowerlimits:
-					if data['Follower_Count'] < ll:
-						sizebucket = sizebuckets[lowerlimits.index(ll) - 1]
-						break
-				if sizebucket is None:
-					sizebucket = sizebuckets[5]
-			#print(data['Follower_Count'], sizebucket)
+	size = data['Follower_Count']
 
-			for ll in lowerlimits:
-				if data['Follower_Count'] is None: #
-					sizebucket = sizebuckets[5] #
-					break #
-				if data['Follower_Count'] < ll:
-					sizebucket = sizebuckets[lowerlimits.index(ll) - 1]
-					print(data['Follower_Count'], sizebucket)
+	posts = data['Posts']
+	number_of_posts = len(posts)
+
+	engagements_total = 0
+	ratio_total = 0
+
+	for post in posts:
+		post_engagements, post_ratio = post_stats(post, size, platform)
+		if post_ratio is None:
+			number_of_posts -= 1
+		else:
+			engagements_total += post_engagements
+			ratio_total += post_ratio
+
+	if engagements_total is 0:
+		engagement_average = None
+	else:
+		engagement_average = engagements_total/float(number_of_posts)
+	
+	if ratio_total is 0:
+		ratio_average = None
+	else:
+		ratio_average = ratio_total/float(number_of_posts)
+
+	return size, engagement_average, ratio_average
+
+def calculate_stats(genres, names, usernames, dirname, platform):
+	
+	"""
+	Arguments:
+		genres: A list of genre strings, ordered as the profiles were read in
+		names: A list of artist name strings, ordered as the profiles were read in
+		usernames: A list of username (i.e. channel name) strings, ordered as the profiles were read in
+		dirname: The name of the directory profile data can be read from.
+		platform: The name of the platform the data comes from.
+	"""
+
+	all_genres = {}
+	for genre in GENRES:
+		all_genres[genre] = []
+	all_sizes = {}
+	for size in SIZES:
+		all_sizes[size] = []
+	all_engagements = {}
+	all_ratios = {}
+
+	for name, username, artist_genres in zip(names, usernames, genres):
+		size, engagement, ratio = profile_stats(name, dirname, platform)
+
+		if size is None or engagement is None or ratio is None:
+			continue
+		
+		if size >= LIMITS[-1]:
+			all_sizes[SIZES[-1]].append(name)
+		else:
+			for limit in LIMITS:
+				if size < limit:
+					all_sizes[SIZES[LIMITS.index(limit) - 1]].append(name)
 					break
 
-			nposts.append(len(data['Posts']))
-			postengagements = []
-			for post in data['Posts']:
-				theirengagements = 0
-				theirengagements += post['shares']
-				theirengagements += post['comments']
-				for reaction in post['reactions']:
-					theirengagements += post['reactions'][reaction]
-				postengagements.append(theirengagements)
-			if len(postengagements) > 0:
-				engagementratio = sum(postengagements)/float(len(postengagements))
-				engagements.append(engagementratio)
-				for genre in genrelist.split('/'):
-					if genre in genrebuckets:
-						#print(name, genre, sizebucket)
-						engagementslists[genre][sizebucket].append(engagementratio)
-			else:
-				engagements.append(None)
+		for genre in artist_genres:
+			all_genres[genre].append(name)
 
-			for genre in genrelist.split('/'):
-				if genre in genrebuckets:
-					npostslists[genre][sizebucket].append(len(data['Posts']))
-			#print(name, len(data['Posts']))
-			#print(name, len(postengagements))
+		all_engagements[name] = engagement
+		all_ratios[name] = ratio
 
-	#print(npostslists)
-	#print(engagementslists)
+	stats = {}
+	for genre in GENRES:
+		stats[genre] = {}
 
-	for genrebucket in genrebuckets:
-		for sizebucket in sizebuckets:
-			if float(len(npostslists[genrebucket][sizebucket])) > 0:
-				avgnposts[genrebucket][sizebucket] = sum(npostslists[genrebucket][sizebucket])/float(len(npostslists[genrebucket][sizebucket]))
-			else: 
-				avgnposts[genrebucket][sizebucket] = 0
+	for genre in GENRES:
+		for size in SIZES:
+			bucket_artists = set(all_genres[genre]).intersection(all_sizes[size])
+			bucket_engagement = sum([all_engagements[artist] for artist in bucket_artists])/float(len(bucket_artists))
+			bucket_ratio = sum([all_ratios[artist] for artist in bucket_artists])/float(len(bucket_artists))
+			stats[genre][size] = (bucket_engagement, bucket_ratio)
 
-			if float(len(engagementslists[genrebucket][sizebucket])) > 0:
-				avgengagements[genrebucket][sizebucket] = sum(engagementslists[genrebucket][sizebucket])/float(len(engagementslists[genrebucket][sizebucket]))
-			else:
-				avgengagements[genrebucket][sizebucket] = 0
+	return all_genres, all_sizes, all_engagements, all_ratios, stats
 
-			#print(len(npostslists[genrebucket][sizebucket]), len(engagementslists[genrebucket][sizebucket]))
+def save_stats(fname, all_genres, all_sizes, all_engagements, all_ratios, stats):
+	
+	"""
+	Arguments:
+		fname: The .csv file to save the stats to.
+		all_genres: A dictionary mapping genres to a list of names of artists within that genre
+		all_sizes: A dictionary mapping audience sizes to a list of names of artists with those audience sizes
+		all_engagements: A dictionary mapping names of artists to their raw engagement number
+		all_ratios: A dictionary mapping names of artists to their engagement ratio
+		stats: A dictionary of dictionaries mapping genres to buckets to the (engagement number & ratio for that bucket)
+	"""
 
-
-	with open(args.results, 'wb') as logfile:
+	with open(fname, 'w') as logfile:
 		wr = csv.writer(logfile)
-		wr.writerow(['Genre', 'Name', 'Username', 'Follower Count', 'Number of Posts', 'Average Engagements/Post'])
-		for genre, name, username, size, npost, engagement in zip(genres, names, usernames, sizes, nposts, engagements):
-			wr.writerow([genre, name, username, size, npost, engagement])
+		wr.writerow(['Genre', 'Size', 'Name', 'Average Engagements/Post', 'Average Engagement Ratio'])
+		for genre in GENRES:
+			for size in SIZES:
+				bucket_artists = set(all_genres[genre]).intersection(all_sizes[size])
+				for artist in bucket_artists:
+					wr.writerow([genre, size, artist, all_engagements[artist], all_ratios[artist]])
+
 		wr.writerow([])
-		for genrebucket in genrebuckets:
-			for sizebucket in sizebuckets:
-				wr.writerow([genrebucket, '', '', sizebucket, avgnposts[genrebucket][sizebucket], avgengagements[genrebucket][sizebucket]])
+		for genre in GENRES:
+			for size in SIZES:
+				wr.writerow([genre, size, '', stats[genre][size][0], stats[genre][size][1]])
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 	main()
